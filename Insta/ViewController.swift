c7f004f3d38a08c867edad97eea3a1ba32ec102e
Антон Коротков <byfrunze@gmail.com>
2022-02-02 15:13:39 +0400

    Initial Commit

---
+++ Insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by Антон Коротков on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
